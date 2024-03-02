6058e71ee9fc6e50644031fccd84e3cbf7dd6cf4
Toxa <untoxa@mail.ru>
2024-03-02 10:47:07 +0300

    use "mirror" aliases to unify with the rest of the code

--- include/ZGBMain.h
+++ include/ZGBMain.h
@@ -8,11 +8,11 @@ _STATE(StateCoverWin)\
 STATE_DEF_END
 
 #define SPRITES \
-_SPRITE_DMG_FLIP(SpritePlayer, helo, FLIP_X)\
+_SPRITE_DMG_MIRROR(SpritePlayer, helo, V_MIRROR)\
 _SPRITE_DMG(SpriteGancho, gancho)\
 _SPRITE_DMG(SpriteRope, bolita)\
 _SPRITE_DMG(SpritePollo, pollo)\
-_SPRITE_DMG_FLIP(SpriteTurbo, heloturbo, FLIP_X)\
+_SPRITE_DMG_MIRROR(SpriteTurbo, heloturbo, V_MIRROR)\
 _SPRITE_DMG(SpriteMouth, monster)\
 SPRITE_DEF_END
 
